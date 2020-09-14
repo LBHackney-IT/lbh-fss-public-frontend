c0e263f623d53117fd964bb3def35337063e5574
Sam Wong <sam.wong@nudgedigital.co.uk>
2020-09-14 16:01:43 +0100

    Set prevs for listservices

--- src/components/Service/ListServices.js
+++ src/components/Service/ListServices.js
@@ -2,13 +2,31 @@ import React, { useEffect, useState, useContext } from 'react';
 import GetServices from "../../services/GetServices/GetServices";
 import ServiceCard from "./ServiceCard";
 import { CardContainer } from "../../util/styled-components/CardContainer";
+import PrevUrlContext from "../../context/PrevUrlContext/PrevUrlContext";
 import UrlParamsContext from "../../context/UrlParamsContext/UrlParamsContext";
+import PrevUrlParamsContext from "../../context/PrevUrlParamsContext/PrevUrlParamsContext";
 import Header from "../Header/Header";
 
 const ListServices = ({ categories = [], onClick }) => {
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const {urlParams, setUrlParams} = useContext(UrlParamsContext);
+  const {prevUrl, setPrevUrl} = useContext(PrevUrlContext);
+  const {prevUrlParams, setPrevUrlParams} = useContext(PrevUrlParamsContext);
+  const paramsArray = ["category_explorer", "postcode", "service_search", "service", "categories", "demographic"];
+  const currentSearch = window.location.search;
+  let paramObj = {};
+
+  function createParamObj(currentSearch, paramsArray) {
+    const queryParts = currentSearch.substring(1).split(/[&;]/g);
+    const arrayLength = queryParts.length;
+    for (let i = 0; i < arrayLength; i++) {
+        const queryKeyValue = queryParts[i].split("=");
+        if (paramsArray.includes(queryKeyValue[0])) {
+            paramObj[queryKeyValue[0]] = queryKeyValue[1];
+        } 
+    }
+  }
 
   useEffect(() => {
     async function fetchData() {
@@ -20,6 +38,23 @@ const ListServices = ({ categories = [], onClick }) => {
       setIsLoading(false);
     }
     fetchData();
+
+    if (prevUrl == 0 && prevUrlParams == 0) {
+      let prevUrlArray = [""];
+      let prevUrlParamsArray = [{}];
+
+      // setPrevUrl
+      if (currentSearch) {
+        prevUrlArray.push(currentSearch);
+        setPrevUrl(prevUrlArray);
+      }
+
+      // setPrevUrlParams
+      createParamObj(currentSearch, paramsArray);
+      prevUrlParamsArray.push(paramObj);
+      setPrevUrlParams(prevUrlParamsArray);
+
+    }
 
   }, [setData, setIsLoading]);
 
