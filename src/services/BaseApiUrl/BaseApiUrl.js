6e2cba1d2f96a0079a9748ad2e8d44b5942c15e0
Sam Wong <sam.wong@nudgedigital.co.uk>
2020-12-10 20:27:16 +0800

    Add production URL

--- src/services/BaseApiUrl/BaseApiUrl.js
+++ src/services/BaseApiUrl/BaseApiUrl.js
@@ -1,3 +1,7 @@
+// staging
+// const BASE_API_URL =
+//     "https://xx0z3bot52.execute-api.eu-west-2.amazonaws.com/staging/api/v1";
+// production
 const BASE_API_URL =
-    "https://xx0z3bot52.execute-api.eu-west-2.amazonaws.com/staging/api/v1";
+    "https://b56cfvfyb8.execute-api.eu-west-2.amazonaws.com/production/api/v1";
 export default BASE_API_URL;
