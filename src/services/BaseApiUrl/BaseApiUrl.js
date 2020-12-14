4f8e1f84a7ab81629dd91cc985dc81c23e3fe57f
James South <james.south@nudgedigital.co.uk>
2020-12-14 14:54:20 +0000

    Update to latest production API URL

--- src/services/BaseApiUrl/BaseApiUrl.js
+++ src/services/BaseApiUrl/BaseApiUrl.js
@@ -3,5 +3,5 @@
 //     "https://xx0z3bot52.execute-api.eu-west-2.amazonaws.com/staging/api/v1";
 // production
 const BASE_API_URL =
-    "https://b56cfvfyb8.execute-api.eu-west-2.amazonaws.com/production/api/v1";
+    "https://qtv3g90mcf.execute-api.eu-west-2.amazonaws.com/production/api/v1";
 export default BASE_API_URL;
