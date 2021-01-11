b3f65ded397f5c5098875f812b6039bf9be41bdc
Sam Wong <sam.wong@nudgedigital.co.uk>
2021-01-11 11:38:18 +0800

    Update baseurl

--- src/services/BaseApiUrl/BaseApiUrl.js
+++ src/services/BaseApiUrl/BaseApiUrl.js
@@ -1,7 +1,15 @@
-// staging
-// const BASE_API_URL =
-//     "https://xx0z3bot52.execute-api.eu-west-2.amazonaws.com/staging/api/v1";
-// production
-const BASE_API_URL =
-    "https://qtv3g90mcf.execute-api.eu-west-2.amazonaws.com/production/api/v1";
+let BASE_API_URL = "";
+
+const location = window.location.origin;
+
+if (location.includes("find-support-services.hackney")) {
+  BASE_API_URL = `https://qtv3g90mcf.execute-api.eu-west-2.amazonaws.com/production/api/v1`;
+} else if (location.includes("find-support-services-staging.hackney")) {
+  BASE_API_URL = `https://xx0z3bot52.execute-api.eu-west-2.amazonaws.com/staging/api/v1`;
+} else {
+  // BASE_API_URL = `https://qtv3g90mcf.execute-api.eu-west-2.amazonaws.com/production/api/v1`;
+  BASE_API_URL = `https://xx0z3bot52.execute-api.eu-west-2.amazonaws.com/staging/api/v1`;
+//   BASE_API_URL = "/api";
+}
+
 export default BASE_API_URL;
